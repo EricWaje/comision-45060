0691eda091e67dde61f2887c8c48fac3d42e5ad4
Eric Wajnrajch <ericwaje03@gmail.com>
2022-10-12 20:36:54 -0300

    after 12-10

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import './App.css';
+//import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.scss';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main/Main';
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,34 +1,77 @@
 import estilos from './main.module.css';
 
-const styles = {
-    parrafo: {
-        color: 'gold',
-        backgroundColor: 'blue',
-    },
-    contenedor: {
-        color: 'green',
-        backgroundColor: 'yellow',
-    },
-};
-
 const Main = () => {
     return (
-        <main className={estilos.navbar}>
-            <p style={{ color: 'blue' }}>
+        <main
+            className={estilos.main}
+            style={{ display: 'flex', justifyContent: 'center' }}
+        >
+            <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Perspiciatis ducimus illo dolor blanditiis excepturi natus nam
-                aliquam sed error delectus, similique ipsum provident molestiae
-                doloribus commodi, eligendi officia labore omnis.
+                Dignissimos placeat perferendis quidem reprehenderit maiores
+                nostrum neque illo voluptatum voluptate, odit animi consequatur
+                quo pariatur perspiciatis quis fuga recusandae voluptas rem!
             </p>
-            <p style={styles.parrafo}>Bienvenidos a la clase 3</p>
-            <div style={styles.contenedor}>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-                tenetur deserunt atque in quidem distinctio odio vero ratione,
-                accusamus veritatis, necessitatibus officiis. Corrupti impedit
-                saepe maxime temporibus. Sed, nulla eius!
-            </div>
         </main>
     );
 };
 
 export default Main;
+
+//Botstrap de la manera vieja
+/* <div className="card" style={{ width: '12rem' }}>
+    <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6ykev_1vBYJZO79NxTpWfMC3HQTJchCFWNA&usqp=CAU"
+        className="card-img-top"
+        alt="..."
+    />
+    <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+        </p>
+        <a href="https://google.com" className="btn btn-primary">
+            Go somewhere
+        </a>
+    </div>
+</div>; */
+
+//React Bootstrap
+
+/* <Container>
+    <Row>
+        <Col md={4}>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img
+                    variant="top"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6ykev_1vBYJZO79NxTpWfMC3HQTJchCFWNA&usqp=CAU"
+                />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </Col>
+        <Col md={4}>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img
+                    variant="top"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6ykev_1vBYJZO79NxTpWfMC3HQTJchCFWNA&usqp=CAU"
+                />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </Col>
+    </Row>
+</Container>; */

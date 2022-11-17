4eb77bd7cf09121ce3a43e184ef089371f585581
Eric Wajnrajch <ericwaje03@gmail.com>
2022-11-17 20:36:05 -0300

    guardando credenciales

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ import CartProvider from './context/CartContext';
 
 //addDoc
 
+//console.log(process.env);
+
 const App = () => {
     return (
         <BrowserRouter>
--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyAV-dV7mWAomzpK2ZlRGdf7PHoiL6uXVQA',
-    authDomain: 'react-45060-profe.firebaseapp.com',
-    projectId: 'react-45060-profe',
-    storageBucket: 'react-45060-profe.appspot.com',
-    messagingSenderId: '376958279021',
-    appId: '1:376958279021:web:07d045fd56cc0006cace64',
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE,
+    messagingSenderId: process.env.REACT_APP_MESSAGING,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
